ef13369e3832af1ca5608b40600d2cafc38d0b32
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-10-15 23:24:43 +0530

    feat:express basic server init

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express = require("express");
 
 const app= express();
 
+// initiate other services
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, ()=> {
+    console.log("connected to MongoDB");
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// app.get("/", (req,res)=> {
+//     res.send("Welcome to homepage");
+// })
+
 app.listen(8800, ()=> {
     console.log("backend server running");
 })
